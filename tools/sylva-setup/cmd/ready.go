94ee15486431e1c6f35a759b4426e1d22ee0a1d2
Benjamin Le Diguerher <benjamin.lediguerher@orange.com>
2024-08-08 09:54:45 +0000

    first draft of dev ready command

--- tools/sylva-setup/cmd/ready.go
+++ tools/sylva-setup/cmd/ready.go
@@ -1,34 +1,47 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Antoine Monlong <antoine.monlong@orange.com>
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package cmd
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"archive/tar"
-	"compress/gzip"
+
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
 // readyCmd represents the ready command
 var readyCmd = &cobra.Command{
 	Use:   "ready",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Set up your development environment",
+	Long: `This command sets up your development environment by installing necessary packages and
+cloning Sylva Git repository.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ready called")
 		hello_world()
+		if err := setupDevEnv(); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("Your dev environment is ready. If you don't know what to do next, you can go to the doc <link to the doc>")
 	},
 }
 
@@ -48,16 +61,107 @@ func init() {
 
 func hello_world() {
 	fmt.Println("Hello world.")
-	tu arrives chez Orange/TelecomItalia/etc., tu dois installer sylva
-
-	install les requirements
-	git clone sylvacore  (main)
-	print ouvre la doc
-
 }
 
+type Package struct {
+	Name   string
+	Type   string
+	URL    string
+	Binary string
+}
 
-// downloadFile downloads a file from a URL and saves it to a local path.
+func init() {
+	devCmd.AddCommand(readyCmd)
+}
+
+func setupDevEnv() error {
+	// name, type, url, command
+	packages := []Package{
+		{"apt", "apt", "", "apt"},
+		{"python3", "apt", "", "python3"},
+		{"git", "apt", "", "git"},
+		{"docker.io", "apt", "", "docker"},
+		{"yq", "apt", "", "yq"},
+		{"yamllint", "apt", "", "yamllint"},
+		{"pip3", "apt", "", "pip3"},
+		{"PyYAML", "pip", "", "pyyaml"},
+		{"k9s", "go-binary", "https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Freebsd_amd64.tar.gz", "k9s"},
+	}
+
+	installed, notInstalled := checkPackages(packages)
+
+	fmt.Println("Installed packages:")
+	for _, pkg := range installed {
+		fmt.Println("-", pkg.Name)
+	}
+
+	fmt.Println("\nNot installed packages:")
+	for _, pkg := range notInstalled {
+		fmt.Println("-", pkg.Name)
+	}
+
+	prompt := promptui.Prompt{
+		Label:     "Would you like to install all missing packages? (y/n)",
+		IsConfirm: true,
+	}
+	result, err := prompt.Run()
+	if err != nil {
+		return fmt.Errorf("failed to read user input: %w", err)
+	}
+	fmt.Println(result)
+	if result != "y" {
+		fmt.Println("You'll need those package to continue. Exiting...")
+		return nil
+	}
+
+	fmt.Println("downloading packages...")
+	// for _, pkg := range notInstalled {
+	// 	if err := installPackage(pkg); err != nil {
+	// 		return fmt.Errorf("failed to install %s: %w", pkg.Name, err)
+	// 	}
+	// }
+	fmt.Println("cloning sylva core repository...")
+	// if err := cloneRepository("https://gitlab.com/sylva-projects/sylva-core.git", "sylva-core"); err != nil {
+	// 	return fmt.Errorf("failed to clone repository: %w", err)
+	// }
+
+	return nil
+}
+
+func checkPackages(packages []Package) (installed, notInstalled []Package) {
+	for _, pkg := range packages {
+		if isInstalled(pkg.Name) {
+			installed = append(installed, pkg)
+		} else {
+			notInstalled = append(notInstalled, pkg)
+		}
+	}
+	return
+}
+
+func isInstalled(binary string) bool {
+	_, err := exec.LookPath(binary)
+	return err == nil
+}
+
+func installPackage(pkg Package) error {
+	fmt.Printf("Installing %s...\n", pkg.Name)
+	switch pkg.Type {
+	case "apt":
+		return exec.Command("sudo", "apt-get", "install", "-y", pkg.Name).Run()
+	case "pip":
+		return exec.Command("pip3", "install", pkg.Name).Run()
+	case "go-binary":
+		dest := "/tmp/" + filepath.Base(pkg.URL)
+		if err := downloadFile(pkg.URL, dest); err != nil {
+			return err
+		}
+		return extractTarGzFile(dest, "/usr/local/bin")
+	default:
+		return fmt.Errorf("unknown package type: %s", pkg.Type)
+	}
+}
+
 func downloadFile(url, dest string) error {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -75,7 +179,16 @@ func downloadFile(url, dest string) error {
 	return err
 }
 
-// extractTarGz extracts a .tar.gz file to a specified destination.
+func extractTarGzFile(src, dest string) error {
+	file, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return extractTarGz(file, dest)
+}
+
 func extractTarGz(gzipStream io.Reader, dest string) error {
 	uncompressedStream, err := gzip.NewReader(gzipStream)
 	if err != nil {
@@ -114,69 +227,10 @@ func extractTarGz(gzipStream io.Reader, dest string) error {
 	return nil
 }
 
-// installPackage installs a package using apt-get.
-func installPackage(pkg string) error {
-	cmd := exec.Command("sudo", "apt-get", "install", "-y", pkg)
+func cloneRepository(repoURL, dest string) error {
+	fmt.Printf("Cloning repository %s to %s...\n", repoURL, dest)
+	cmd := exec.Command("git", "clone", repoURL, dest)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
-
-// isInstalled checks if a binary is installed.
-func isInstalled(binary string) bool {
-	_, err := exec.LookPath(binary)
-	return err == nil
-}
-
-// installPackages installs the required packages.
-func installPackages() error {
-	packages := map[string]string{
-		"apt":        "apt",
-		"python3":    "python3",
-		"git":        "git",
-		"docker.io":  "docker",
-		"yq":         "yq",
-		"yamllint":   "yamllint",
-		"pip3":       "python3-pip",
-		"PyYAML":     "pip3 install PyYAML",
-	}
-
-	for binary, pkg := range packages {
-		if isInstalled(binary) {
-			fmt.Printf("%s is already installed\n", binary)
-		} else {
-			fmt.Printf("Installing %s...\n", pkg)
-			if binary == "PyYAML" {
-				cmd := exec.Command("pip3", "install", "PyYAML")
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				if err := cmd.Run(); err != nil {
-					return fmt.Errorf("failed to install %s: %w", binary, err)
-				}
-			} else {
-				if err := installPackage(pkg); err != nil {
-					return fmt.Errorf("failed to install %s: %w", binary, err)
-				}
-			}
-		}
-	}
-
-	// Check and install k9s
-	if isInstalled("k9s") {
-		fmt.Println("k9s is already installed")
-	} else {
-		k9sURL := "https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Freebsd_amd64.tar.gz"
-		k9sDest := "/tmp/k9s_Freebsd_amd64.tar.gz"
-		fmt.Println("Downloading k9s...")
-		if err := downloadFile(k9sURL, k9sDest); err != nil {
-			return fmt.Errorf("failed to download k9s: %w", err)
-		}
-		fmt.Println("Extracting k9s...")
-		if err := extractTarGzFile(k9sDest, "/usr/local/bin"); err != nil {
-			return fmt.Errorf("failed to extract k9s: %w", err)
-		}
-	}
-
-	return nil
-}
-
