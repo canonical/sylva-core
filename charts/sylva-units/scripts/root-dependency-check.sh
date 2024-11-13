8bee1dc38f2d351eafa4a17bf437da1309173c00
Thomas Morin <thomas.morin@orange.com>
2024-11-13 15:15:09 +0100

    make root-dependency job quicker

--- charts/sylva-units/scripts/root-dependency-check.sh
+++ charts/sylva-units/scripts/root-dependency-check.sh
@@ -14,22 +14,6 @@
 set -e
 set -o pipefail
 
-
-# how long to wait for all Kustomizations to meet the criteria
-# (we shouldn't need to wait for long, except that if k8s API is slow
-#  it may require some time for kubectl to see all Kustomizations, and
-#  unfortunately it may declare that a Kustomization didn't meet the wait
-#  criteria even if just didn't had time to see it)
-WAIT_TIMEOUT=${WAIT_TIMEOUT:-60s}
-
-# we setup an exit trap to display the status of all Kustomization
-# if one of the 'kubectl wait' fails
-function error_trap() {
-    echo "--- summary of resources"
-    kubectl get Kustomizations -l sylva-units/root-dependency-wait
-}
-trap error_trap ERR
-
 echo "--- deleting leftover root-dependency-<n> Kustomizations for older versions"
 kubectl get Kustomization -l sylva-units.unit=root-dependency -o json | jq -r '.items[] | select(.metadata.annotations."sylva-units-helm-revision" != "'$HELM_REVISION'") | .metadata.name' \
     | xargs -r kubectl delete Kustomization || true
@@ -46,5 +30,7 @@ kubectl get ConfigMap -o json | jq -r '.items[] | select((.metadata.name | start
 
 echo "--- waiting for Kustomizations to be labeled with sylva-units-helm-revision=$HELM_REVISION"
 
-kubectl wait Kustomization -l sylva-units/root-dependency-wait --timeout $WAIT_TIMEOUT \
-  --for=jsonpath="{.metadata.annotations.sylva-units-helm-revision}=$HELM_REVISION"
+kubectl get Kustomizations -o name -l sylva-units/root-dependency-wait \
+ | xargs -n1 -P500 \
+     kubectl wait --timeout 10s \
+                  --for=jsonpath="{.metadata.annotations.sylva-units-helm-revision}=$HELM_REVISION"
