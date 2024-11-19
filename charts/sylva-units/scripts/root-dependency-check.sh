0d8c2a5d116ffb9d5479334f0c898cf7e61930fd
Thomas Morin <thomas.morin@orange.com>
2024-11-19 18:37:47 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Francois Eleouet <francois.eleouet@orange.com>

--- charts/sylva-units/scripts/root-dependency-check.sh
+++ charts/sylva-units/scripts/root-dependency-check.sh
@@ -41,7 +41,7 @@ kubectl get Kustomization -l sylva-units.unit=root-dependency-helm -o json | jq 
 # this is a safeguard:
 echo "--- deleting leftover root-dependency-helm-<n> HelmReleases for older versions"
 kubectl get HelmReleases -o json -l sylva-units.unit=root-dependency-helm | jq -r '.items[] | select(.metadata.labels."sylva-units.version" // "" != "'$HELM_REVISION'") | .metadata.name' \
-    | xargs -r kubectl delete ConfigMap || true
+    | xargs -r kubectl delete HelmRelease || true
 
 # this is a safeguard only relevant when upgrading from a version of sylva before https://gitlab.com/sylva-projects/sylva-core/-/merge_requests/3318
 echo "--- deleting leftover root-dependency-<n>-cm ConfigMaps for older versions"
