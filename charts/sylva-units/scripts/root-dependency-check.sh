eb4f9c98b1999143d57d726d72fee8bd88099387
Thomas Morin <thomas.morin@orange.com>
2024-11-25 16:56:47 +0000

    improve how we prevent unit HelmRelease from reconciling with old state

--- charts/sylva-units/scripts/root-dependency-check.sh
+++ charts/sylva-units/scripts/root-dependency-check.sh
@@ -35,6 +35,11 @@ kubectl get Kustomization -l sylva-units.unit=root-dependency -o json | jq -r '.
     | xargs -r kubectl delete Kustomization || true
 
 # this is a safeguard:
+echo "--- deleting leftover root-dependency-<n> HelmReleases for older versions"
+kubectl get HelmReleases -o json -l sylva-units.unit=root-dependency | jq -r '.items[] | select(.metadata.labels."sylva-units.version" // "" != "'$HELM_REVISION'") | .metadata.name' \
+    | xargs -r kubectl delete HelmRelease || true
+
+# this is a safeguard only relevant when upgrading from a version of sylva before https://gitlab.com/sylva-projects/sylva-core/-/merge_requests/3318
 echo "--- deleting leftover root-dependency-<n>-cm ConfigMaps for older versions"
 kubectl get ConfigMap -o json | jq -r '.items[] | select((.metadata.name | startswith("root-dependency-")) and (.metadata.labels."sylva-units.version" // "" != "'$HELM_REVISION'")) | .metadata.name' \
     | xargs -r kubectl delete ConfigMap || true
