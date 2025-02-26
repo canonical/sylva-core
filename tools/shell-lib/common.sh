275248a779322769960c96e144496f29ffefeeee
sakshi <sakuchoudhary98@gmail.com>
2025-02-26 13:59:01 +0530

    remove sylvactl version

--- tools/shell-lib/common.sh
+++ tools/shell-lib/common.sh
@@ -200,8 +200,6 @@ function ensure_sylvactl {
         curl -q --progress-bar -f https://gitlab.com/api/v4/projects/43501695/packages/generic/$SYLVACTL_RELEASE_NAME/$SYLVACTL_VERSION/sylvactl -o ${USER_DIR}/bin/sylvactl
         chmod +x ${USER_DIR}/bin/sylvactl
     fi
-    curl -q --progress-bar -f https://gitlab.com/api/v4/projects/43501695/packages/generic/sylvactl-unstable/0.0.0-git-8a32d726/sylvactl -o ${BASE_DIR}/bin/sylvactl
-    chmod +x ${BASE_DIR}/bin/sylvactl
 }
 
 function cleanup_bootstrap_cluster() {
