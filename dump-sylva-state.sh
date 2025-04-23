c0c0a10f9ac73d44499c6a48aae18704a5b30748
Mederic de Verdilhac <mederic.deverdilhac@orange.com>
2025-04-23 18:44:55 +0200

    dump-sylva-state.sh - Store dump files in a dedicated directory

--- dump-sylva-state.sh
+++ dump-sylva-state.sh
@@ -3,26 +3,25 @@
 # This script can be used to dump sylva clusters state (bootstrap/management/workload)
 # it generates an archive 'sylva-dump-YYYY-MM-DDThh-mm-ss.tar.gz' which may shared (in issue by example)
 
+USER_PWD=$(pwd)
+DUMPS_DIR="$USER_PWD/dumps"
+CURRENT_DUMP_DIR="$USER_PWD/dumps/current-dump"
+
 source $(dirname $(realpath $0))/tools/shell-lib/common.sh
 
 ensure_sylva_toolbox
 
+rm -rf "$CURRENT_DUMP_DIR"
+mkdir -p "$CURRENT_DUMP_DIR"
+cd "$CURRENT_DUMP_DIR"
+
 echo_b "👀 Dumping sylva state"
 ${BASE_DIR}/tools/shell-lib/debug-on-exit.sh 2>&1 | tee sylva-dump.log
 
-archive_name="sylva-dump-$(date -u '+%Y%M%d-%H%M%S').tar.gz"
-contents=""
-[ -d system-dump ] && contents="${contents} system-dump"
-[ -d bootstrap-cluster-dump ] && contents="${contents} bootstrap-cluster-dump"
-[ -d management-cluster-dump ] && contents="${contents} management-cluster-dump"
-[ -d workload-cluster-dump ] && contents="${contents} workload-cluster-dump"
-[ -f crust-gather.tar.gz ] && contents="${contents} crust-gather.tar.gz"
-[ -f sylva-dump.log ] && contents="${contents} sylva-dump.log"
+archive_name="$DUMPS_DIR/sylva-dump-$(date -u '+%Y%m%d-%H%M%S').tar.gz"
+relative_archive_name=$(realpath -s --relative-to="$USER_PWD" "$archive_name")
 
-if [ -n "$contents" ]; then
-  echo_b "🎁 Creating archive $archive_name"
-  tar -czf "$archive_name" ${contents}
-  echo "File $archive_name is ready"
-else
-  echo "No archive to create"
-fi
+echo_b "🎁 Creating archive $relative_archive_name"
+ls
+tar -czf "$archive_name" "."
+echo "File $relative_archive_name is ready"
